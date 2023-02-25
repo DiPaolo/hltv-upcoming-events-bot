af8f913106d0764098e5f708459c88e42fb533fe
Pavel Ditenbir <pavel.ditenbir@gmail.com>
2023-02-25 11:18:28 +0700

    bump version: 1.1.3

--- hltv_upcoming_events_bot/__version__.py
+++ hltv_upcoming_events_bot/__version__.py
@@ -1,4 +1,4 @@
-__version__ = "1.1.1"
+__version__ = "1.1.3"
 __author__ = "Pavel 'DiPaolo' Ditenbir"
 __author_email__ = "pavel.ditenbir@gmail.com"
 __copyright__ = "Copyright Pavel 'DiPaolo' Ditenbir"
