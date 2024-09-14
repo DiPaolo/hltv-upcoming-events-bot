407f1899a30f92022dbdc199ddf57cb71f3c9baf
Pavel Ditenbir <pavel.ditenbir@gmail.com>
2024-09-14 11:06:29 +0700

    add error handling of TG updates

--- hltv_upcoming_events_bot/bot/bot.py
+++ hltv_upcoming_events_bot/bot/bot.py
@@ -61,19 +61,47 @@ def unsubscribe_command(engine: Update, context: CallbackContext) -> None:
 
 
 def help_command(engine: Update, context: CallbackContext) -> None:
-    log_command(engine)
+    log_command(engine, _logger)
     engine.message.reply_text(_get_help_text(), parse_mode=ParseMode.HTML)
 
 
 def version_command(engine: Update, context: CallbackContext) -> None:
-    log_command(engine)
+    log_command(engine, _logger)
     engine.message.reply_text(hltv_upcoming_events_bot.__version__, parse_mode=ParseMode.HTML)
 
 
 def send_message(chat_id: int, msg: str):
     bot = telegram.Bot(config.BOT_TOKEN)
-    ret = bot.send_message(chat_id=chat_id, text=msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
-    pass
+    try:
+        bot.send_message(chat_id=chat_id, text=msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+    except telegram.error.TelegramError as ex:
+        _logger.error(f'failed to send message (chat_id={chat_id}): {ex}')
+
+
+def error_handler(update: object, context: CallbackContext) -> None:
+    """Log the error and send a telegram message to notify the developer."""
+    # Log the error before we do anything else, so we can see it even if something breaks.
+    _logger.error(msg="Exception while handling an update: ", exc_info=context.error)
+
+    # traceback.format_exception returns the usual python message about an exception, but as a
+    # list of strings rather than a single string, so we have to join them together.
+    # tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    # tb_string = ''.join(tb_list)
+
+    # Build the message with some markup and additional information about what happened.
+    # You might need to add some logic to deal with messages longer than the 4096 character limit.
+    # update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    # message = (
+    #     f'An exception was raised while handling an update\n'
+    #     f'<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}'
+    #     '</pre>\n\n'
+    #     f'<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n'
+    #     f'<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n'
+    #     f'<pre>{html.escape(tb_string)}</pre>'
+    # )
+
+    # Finally, send the message
+    # context.bot.send_message(chat_id=DEVELOPER_CHAT_ID, text=message, parse_mode=ParseMode.HTML)
 
 
 def start(token: str) -> None:
@@ -97,6 +125,8 @@ def start(token: str) -> None:
     dispatcher.add_handler(CommandHandler("unsubscribe", unsubscribe_command))
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(CommandHandler("version", version_command))
+
+    dispatcher.add_error_handler(error_handler)
 
     engine.start_polling()
     engine.idle()
